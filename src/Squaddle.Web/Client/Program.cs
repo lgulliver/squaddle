c25cd3cdeea89de00ff7c51b323f5af5e83af6d5
Liam Gulliver <liamgulliver@gmail.com>
2020-05-23 00:15:04 +0100

    replace project with hosted version

--- src/Squaddle.Web/Client/Program.cs
+++ src/Squaddle.Web/Client/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using BlazorStrap;
 
-namespace Squaddle.Web
+namespace Squaddle.Web.Client
 {
     public class Program
     {
