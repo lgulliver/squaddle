9ec8256ef42da3e27a8d55d0dd275258af6c89a8
Liam Gulliver <liamgulliver@gmail.com>
2020-05-31 19:31:17 +0100

    join progress. Issue #1 is present

---
+++ src/Squaddle.Shared/Models/Room.cs
@@ -0,0 +1,15 @@
+﻿using Squaddle.Shared.Resources;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Squaddle.Shared.Models
+{
+    public class Room
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string Name { get; set; }
+        public RoomType RoomType { get; set; }
+    }
+}
